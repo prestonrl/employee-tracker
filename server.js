e0ddde397d5cd5840e3429dbf1fd63458acd0f8b
tubasthebest <tubasthebest@gmail.com>
2021-02-07 13:46:41 -0700

    functions to update tables

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ function viewDepartments() {
     console.table(res);
     interface();
   });
-}
+};
+
 function viewRoles() {
   const query = "SELECT * FROM role";
   connection.query(query, function (err, res) {
@@ -68,7 +69,8 @@ function viewRoles() {
     console.table(res);
     interface();
   });
-}
+};
+
 function viewEmployees() {
   const query = "SELECT * FROM employee";
   connection.query(query, function (err, res) {
@@ -76,9 +78,109 @@ function viewEmployees() {
     console.table(res);
     interface();
   });
-}
-function addDepartment() {}
-function addRole() {}
-function addEmployee() {}
-function updateRole() {}
+};
 
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the name of the department you want to add?",
+      name: "department",
+    })
+    .then(function (res) {
+      const department = res.department;
+      const query = `INSERT INTO department (name) VALUES("${department}")`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        interface();
+      });
+    });
+};
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the job title you want to add?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is the salary for this position?",
+        name: "salary",
+      },
+      {
+        type: "input",
+        message: "What is the department ID for this position?",
+        name: "departmentID",
+      },
+    ])
+    .then(function (res) {
+      const title = res.title;
+      const salary = res.salary;
+      const departmentID = res.departmentID;
+      const query = `INSERT INTO role (title, salary, department_id) VALUE("${title}", "${salary}", "${departmentID}")`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        interface();
+      });
+    });
+};
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's role ID?",
+        name: "roleID",
+      },
+      {
+        type: "input",
+        message: "What is the employee's manager ID?",
+        name: "managerID",
+      },
+    ])
+    .then(function (res) {
+      const firstName = res.firstName;
+      const lastName = res.lastName;
+      const roleID = res.roleID;
+      const managerID = res.managerID;
+      const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE("${firstName}", "${lastName}", "${roleID}", "${managerID}")`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        interface();
+      });
+    });
+};
+
+function updateRole() {
+  const query = "SELECT id, first_name, last_name, role_id  FROM employee";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    {
+      inquirer.prompt({
+        type: "input",
+        message:
+          "Which employee needs to be updated? (please use number from id column only)",
+        name: "employee",
+      });
+    }
+    interface();
+  });
+};
