9d1c2ef431e3bbd021a4e41bf81195d499214fdb
tubasthebest <tubasthebest@gmail.com>
2021-02-07 11:37:41 -0700

    schema set up

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS tracker;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE database tracker;
+
+USE tracker;
+
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER
+);
